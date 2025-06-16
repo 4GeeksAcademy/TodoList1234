3c39d890dea21b19a356f6dd2395a591dc09e686
SergioSanz94x <sergiosanz094@gmail.com>
2025-06-16 20:04:48 +0000

    final-to-do-list

--- src/js/components/Home.jsx
+++ src/js/components/Home.jsx
@@ -7,8 +7,6 @@ import rigoImage from "../../img/rigo-baby.jpg";
 const Home = () => {
       const [inputValue, setInputValue] = useState("");
 	  const [todos, setTodos] = useState([]);
-      
-	  
 
 	return (
 		<div className="container">
@@ -18,7 +16,8 @@ const Home = () => {
 			   onChange = {(e)=> setInputValue(e.target.value)}
 			   value = {inputValue}
 			   onKeyPress={(e) => {
-	if (e.key === "Enter" && inputValue.trim() !== "") {
+	if (e.key === "Enter" && inputValue.trim() !== "") 
+		{
 		setTodos(todos.concat(inputValue));
 		setInputValue("");
 	}
